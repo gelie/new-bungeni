cd2dd86908acfacc9f1f0e2296d5ea6c9048d27c
Gavin Elie <gavin.elie@gmail.com>
2026-02-03 09:43:30 +0200

    Loaded DaisyUI and other static assets

--- core/settings.py
+++ core/settings.py
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Custom user model
 AUTH_USER_MODEL = "workflows.User"
